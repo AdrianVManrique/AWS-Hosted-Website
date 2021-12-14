48fa560c476b5317ec68fa394083cb39f19e0ca4
Adrian <adrian@email>
2021-12-13 20:55:53 -0800

    a

--- flask_jwt_rest_server/static/index.js
+++ flask_jwt_rest_server/static/index.js
@@ -9,30 +9,30 @@ function initMap() {
 }
 
 function setPosition(position){
-
+    const myLatlng = { lat: 0.00, lng: 131.044 };
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: position.coords.latitude, lng: position.coords.longitude},
         zoom: 15,
       });
 
     // Create the initial InfoWindow.
-  let infoWindow = new google.maps.InfoWindow({
-    content: "Click the map to get Lat/Lng!",
-    position: myLatlng,
-  });
-
-  infoWindow.open(map);
-  // Configure the click listener.
-  map.addListener("click", (mapsMouseEvent) => {
-    // Close the current InfoWindow.
-    infoWindow.close();
-    // Create a new InfoWindow.
-    infoWindow = new google.maps.InfoWindow({
-      position: mapsMouseEvent.latLng,
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to get Lat/Lng!",
+        position: myLatlng,
     });
-    infoWindow.setContent(
-      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-    );
+
     infoWindow.open(map);
-  });
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+        position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+        JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+    });
 }
