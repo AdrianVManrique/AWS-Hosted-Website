b9515e203443926f0e433101dd110e48ee26a3f9
Adrian <adrian@email>
2021-12-13 19:35:42 -0800

    Finds current position

--- flask_jwt_rest_server/static/index.js
+++ flask_jwt_rest_server/static/index.js
@@ -1,8 +1,21 @@
 let map;
-
+var myLat;
+var myLong;
 function initMap() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition);
+      } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    
+
+    center: { lat: myLat, lng: myLong },
     zoom: 8,
   });
+}
+
+function setPosition(position){
+    myLat = position.coords.latitude;
+    myLong = position.coords.longitude;
 }
