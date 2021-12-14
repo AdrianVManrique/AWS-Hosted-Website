e666888631f86c956114aabbb5fecdc32df0e99a
Adrian <adrian@email>
2021-12-13 20:05:40 -0800

    zoom level

--- flask_jwt_rest_server/static/index.js
+++ flask_jwt_rest_server/static/index.js
@@ -12,6 +12,6 @@ function setPosition(position){
 
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: position.coords.latitude, lng: position.coords.longitude},
-        zoom: 8,
+        zoom: 1,
       });
 }
