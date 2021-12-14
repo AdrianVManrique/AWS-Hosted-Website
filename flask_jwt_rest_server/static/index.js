825aa99e8ee1db95f695d587603d80315366e6d1
Adrian <adrian@email>
2021-12-13 20:51:26 -0800

    aaa

--- flask_jwt_rest_server/static/index.js
+++ flask_jwt_rest_server/static/index.js
@@ -14,4 +14,25 @@ function setPosition(position){
         center: { lat: position.coords.latitude, lng: position.coords.longitude},
         zoom: 15,
       });
+
+    // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: myLatlng,
+  });
+
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
 }
