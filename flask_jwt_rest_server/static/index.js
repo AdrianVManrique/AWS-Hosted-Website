367605062181b6ddbba280d07508e391e5fb90df
Adrian <adrian@email>
2021-12-13 20:01:05 -0800

    c

--- flask_jwt_rest_server/static/index.js
+++ flask_jwt_rest_server/static/index.js
@@ -3,20 +3,14 @@ var myLat;
 var myLong; 
 function initMap() {
     if (navigator.geolocation) {
-        console.log("I AM GETTING COORDS");
-        navigator.geolocation.getCurrentPosition(setPosition);
+        console.log(navigator.geolocation.getCurrentPosition());
       } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
       }
   map = new google.maps.Map(document.getElementById("map"), {
     
 
-    center: { lat: parseFloat(document.getElementById('myLat').value), lng: parseFloat(document.getElementById('myLong').value)},
+    center: { lat: myLat, lng: myLong},
     zoom: 8,
   });
-}
-
-function setPosition(position){
-    myLat = position.coords.latitude;
-    myLong = position.coords.longitude;
 }
